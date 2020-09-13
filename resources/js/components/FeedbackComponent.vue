85ed6660fdd6c5821e92c0cab801d2e8ebb28f2a
SerbanNasui <serban.nasui@gmail.com>
2020-09-13 15:24:00 +0300

    display feedback options

---
+++ resources/js/components/FeedbackComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="text-white">
+        <div v-for="feedback in options" :key="feedback.id"> {{feedback.title}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                options:[{}]
+            }
+        },
+        methods: {
+            getData(){
+                axios.get('api/feedback').then((res)=>{
+                    this.options = res.data
+                })
+            }
+        },
+        mounted() {
+            this.getData()
+        }
+    }
+</script>
